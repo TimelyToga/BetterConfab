ce402ae074c6fde529ccb3987d700a26cc68cd7f
TimelyToga <tblumberg39@gmail.com>
2013-12-21 21:24:12 -0500

    First

---
+++ gen/tsb/timely/toga/betterconfab/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package tsb.timely.toga.betterconfab;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/tsb/timely/toga/betterconfab/Logic.java
@@ -0,0 +1,5 @@
+package tsb.timely.toga.betterconfab;
+
+public class Logic {
+
+}
